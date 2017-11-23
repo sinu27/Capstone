fdd66763f815ec326be193e6d1c98fe9df913f31
sinu27 <shinwoo9105@gmail.com>
2017-11-23 18:56:03 +0900

    DB랑 메뉴 연동완료

--- app/src/main/java/com/example/sinu/capstone/MainActivity.java
+++ app/src/main/java/com/example/sinu/capstone/MainActivity.java
@@ -85,52 +85,61 @@ public class MainActivity extends AppCompatActivity {
         listDataChild = new HashMap<String, List<String>>();
 
 
-        listDataHeader.addAll(al_type);
+        //listDataHeader.addAll(al_type);
         // Adding child data
-//        listDataHeader.add("추천메뉴");
-//        listDataHeader.add("치킨");
-//        listDataHeader.add("한식");
-//        listDataHeader.add("중식");
-//        listDataHeader.add("일식");
+        listDataHeader.add("추천메뉴");
+        listDataHeader.add("한식");
+        listDataHeader.add("일식");
+        listDataHeader.add("튀김");
+        listDataHeader.add("국물");
+        List<String> recom = new ArrayList<String>(),hansik = new ArrayList<String>(),ilsik = new ArrayList<String>(),fries = new ArrayList<String>(),
+                stew = new ArrayList<String>();
 
+        for(int i=0;i<al_type.size();i++) {
+            if (al_type.get(i).toString().equals("추천메뉴")) {
+                recom.add(al_name.get(i).toString()+"|"+al_price.get(i).toString());
+            }else if (al_type.get(i).toString().equals("한식")) {
+                hansik.add(al_name.get(i).toString()+"|"+al_price.get(i).toString());
+            }else if (al_type.get(i).toString().equals("일식")) {
+                ilsik.add(al_name.get(i).toString()+"|"+al_price.get(i).toString());
+            }else if (al_type.get(i).toString().equals("튀김")) {
+                fries.add(al_name.get(i).toString()+"|"+al_price.get(i).toString());
+            }else if (al_type.get(i).toString().equals("국물")) {
+                stew.add(al_name.get(i).toString()+"|"+al_price.get(i).toString());
+            }
+        }
         // Adding child data
-        List<String> recom = new ArrayList<String>();
-        recom.add("김치찌개");
-        recom.add("양념치킨");
-        recom.add("스페셜1");
-        recom.add("닭도리탕");
 
+//        List<String> chicken = new ArrayList<String>();
+//        chicken.add("The Conjuring");
+//        chicken.add("Despicable Me 2");
+//        chicken.add("Turbo");
+//        chicken.add("Grown Ups 2");
+//        chicken.add("Red 2");
+//
+//        List<String> hansik = new ArrayList<String>();
+//        hansik.add("2 Guns");
+//        hansik.add("The Smurfs 2");
+//        hansik.add("The Spectacular Now");
+//        hansik.add("The Canyons");
+//
+//        List<String> jungsik = new ArrayList<String>();
+//        jungsik.add("2 Guns");
+//        jungsik.add("The Smurfs 2");
+//        jungsik.add("The Spectacular Now");
+//
+//
+//        List<String> ilsik = new ArrayList<String>();
+//        ilsik.add("2 Guns");
+//        ilsik.add("The Smurfs 2");
+//        ilsik.add("The Spectacular Now");
+//        ilsik.add("The Canyons");
 
-        List<String> chicken = new ArrayList<String>();
-        chicken.add("The Conjuring");
-        chicken.add("Despicable Me 2");
-        chicken.add("Turbo");
-        chicken.add("Grown Ups 2");
-        chicken.add("Red 2");
-
-        List<String> hansik = new ArrayList<String>();
-        hansik.add("2 Guns");
-        hansik.add("The Smurfs 2");
-        hansik.add("The Spectacular Now");
-        hansik.add("The Canyons");
-
-        List<String> jungsik = new ArrayList<String>();
-        jungsik.add("2 Guns");
-        jungsik.add("The Smurfs 2");
-        jungsik.add("The Spectacular Now");
-
-
-        List<String> ilsik = new ArrayList<String>();
-        ilsik.add("2 Guns");
-        ilsik.add("The Smurfs 2");
-        ilsik.add("The Spectacular Now");
-        ilsik.add("The Canyons");
-
-//        listDataChild.put(listDataHeader.get(0), recom);// Header, Child data
-//        listDataChild.put(listDataHeader.get(1), chicken);
-//        listDataChild.put(listDataHeader.get(2), hansik);
-//        listDataChild.put(listDataHeader.get(3), jungsik);
-//        listDataChild.put(listDataHeader.get(4), ilsik);
+        listDataChild.put(listDataHeader.get(0), recom);// Header, Child data
+        listDataChild.put(listDataHeader.get(1), hansik);
+        listDataChild.put(listDataHeader.get(2), ilsik);
+        listDataChild.put(listDataHeader.get(3), fries);
+        listDataChild.put(listDataHeader.get(4), stew);
     }
 
     @Override
@@ -168,7 +177,11 @@ public class MainActivity extends AppCompatActivity {
         public View getChildView(int groupPosition, final int childPosition,
                                  boolean isLastChild, View convertView, ViewGroup parent) {
 
-            final String childText = (String) getChild(groupPosition, childPosition);
+
+            final String Text = (String) getChild(groupPosition, childPosition);
+            String[] result =Text.split("\\|");
+            final String childText = result[0];
+            final String priceText = result[1];
 
             if (convertView == null) {
                 LayoutInflater infalInflater = (LayoutInflater) this._context
@@ -181,11 +194,16 @@ public class MainActivity extends AppCompatActivity {
 
             txtListChild.setText(childText);
 
+            TextView txtPriceChild = (TextView) convertView
+                    .findViewById(R.id.priceTV);
+
+            txtPriceChild.setText(priceText);
+
             Button button1 = (Button) convertView.findViewById(R.id.button);
             button1.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(_context, "HIHI", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(_context, childText + "  "+priceText, Toast.LENGTH_SHORT).show();
                 }
             });
             return convertView;
